b27bdd9b4237880a8e273b31ef9713340e425980
Pardo7 <walterpardo7@gmail.com>
2019-03-09 16:27:45 -0800

    refactor: deleted repeated bookshelf category components with a single shelf component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,11 +1,17 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Book from './Book';
 
-function ReadBooks(props) {
+function BookShelf(props) {
+	const titles = {
+		currentlyReading: 'Currently Reading',
+		wantToRead: 'Want To Read',
+		read: 'Read'
+	};
+
 	return (
 		<div className="bookshelf">
-			<h2 className="bookshelf-title">Read</h2>
+			<h2 className="bookshelf-title">{titles[props.shelfTitle]}</h2>
 			<div className="bookshelf-books">
 				<ol className="books-grid">
 					{props.books.map(book => <Book key={book.title} book={book} handleShelfChange={props.handleShelfChange} />)}
@@ -15,9 +21,10 @@ function ReadBooks(props) {
 	);
 }
 
-ReadBooks.propTypes = {
+BookShelf.propTypes = {
+	shelfTitle: PropTypes.string.isRequired,
 	books: PropTypes.array.isRequired,
 	handleShelfChange: PropTypes.func.isRequired
 };
 
-export default ReadBooks;
+export default BookShelf;
